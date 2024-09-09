50a50f5cd195e4cc6a90a04129ec6c780b241a0e
dac03 (zmcs) <joaocarlosmottabarbosa@gmail.com>
2024-09-09 09:03:50 -0300

    comit inicial do projeto

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BACKEND_AOP2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Iniciando solicitação...");
+
+            // Criando uma instância do HttpClient
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // Configurando a URL da solicitação
+                    string url = "https://brasilapi.com.br/api/taxas/v1";
+
+                    // Enviando uma solicitação GET
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    // Verificando se a resposta foi bem-sucedida
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Lendo o conteúdo da resposta como string
+                        string responseData = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine("Resposta recebida com sucesso:");
+                        Console.WriteLine(responseData);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Erro na solicitação: " + response.StatusCode);
+                    }
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine("Erro de requisição: " + e.Message);
+                }
+            }
+
+            Console.WriteLine("Finalizando solicitação...");
+            
+        }
+    }
+}
